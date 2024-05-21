cb00e9eda4142088d159ec3caba70fce563886a2
VRik <vrik9449@gmail.com>
2024-05-21 10:10:41 +0300

    fix glider

--- Figure.py
+++ Figure.py
@@ -15,7 +15,8 @@ class Figure:
     def add_figure(self, x, y, cell_manager: CellManager):
         for coordinate in self.cell_coordinates:
             x1, y1 = coordinate
-            cell_manager.cells[y + y1][x + x1].alive = True
+            if  0 < y+y1 < 60 and  0< x+x1 < 80:
+                cell_manager.cells[y + y1][x + x1].alive = True
 
 
 f = Figure('figures/glider.json')
